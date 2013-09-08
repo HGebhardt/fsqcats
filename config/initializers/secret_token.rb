5eba02ffd15751240b493a20ceea50d0ff6e3a33
Hendrik Gebhardt <hendrik.gebhardt@gmail.com>
2013-09-08 10:37:14 +0200

    inital application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fsqcats::Application.config.secret_key_base = 'd710879a97760173853fdd54cc487c6f0ed63d1bac5dd270a73d479ff7ae73aa92a96ba28b3645d93cd196527c61be21def824a1fcf6d0746b0b92339624ed58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fsqcats::Application.config.session_store :cookie_store, key: '_fsqcats_session'
