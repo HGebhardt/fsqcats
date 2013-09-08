041cd85a58a7c9b578c42826883c12f666a3e244
Hendrik Gebhardt <hendrik.gebhardt@gmail.com>
2013-09-08 14:36:27 +0200

    find categories by locale

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.where(locale: params[:locale] || 'de')
   end
 
   # GET /categories/1
